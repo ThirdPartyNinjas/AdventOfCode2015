6a029dec655f553e3db5eccecd7c6b3b6407d9de
Jesse Chounard <jesse@thirdpartyninjas.com>
2015-12-03 23:26:07 -0600

    Added Day 4. Switch to C# because there was no easy built in implementation of md5 for C.

---
+++ Day4.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace AdventOfCode2015_csharp
+{
+	public static class Day4
+	{
+		public static void Part1()
+		{
+			string input = @"bgvyzdsv";
+			int i = 1;
+
+			do
+			{
+				string result = MD5(input + i.ToString());
+				if(result.Substring(0, 5) == "00000")
+					break;
+				i++;
+			} while (true);
+
+			Console.WriteLine(string.Format("d4p1: The lowest number to get leading \"00000\" is: {0}", i));
+        }
+
+		public static void Part2()
+		{
+			string input = @"bgvyzdsv";
+			int i = 1;
+
+			do
+			{
+				string result = MD5(input + i.ToString());
+				if (result.Substring(0, 6) == "000000")
+					break;
+				i++;
+			} while (true);
+
+			Console.WriteLine(string.Format("d4p2: The lowest number to get leading \"000000\" is: {0}", i));
+		}
+
+		// copied directly from stackoverflow
+		// http://stackoverflow.com/questions/11454004/calculate-a-md5-hash-from-a-string
+		private static string MD5(string data)
+		{
+			byte[] encodedPassword = new UTF8Encoding().GetBytes(data);
+
+			// need MD5 to calculate the hash
+			byte[] hash = ((HashAlgorithm)CryptoConfig.CreateFromName("MD5")).ComputeHash(encodedPassword);
+
+			// string representation (similar to UNIX format)
+			string encoded = BitConverter.ToString(hash)
+			   // without dashes
+			   .Replace("-", string.Empty)
+			   // make lowercase
+			   .ToLower();
+
+			return encoded;
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace AdventOfCode2015_csharp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Day4.Part1();
+			Day4.Part2();
+		}
+	}
+}
