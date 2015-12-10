9ec566e6230b4d9f3b37a9cdaeab1bb6df2d8cbe
Jesse Chounard <jesse@thirdpartyninjas.com>
2015-12-09 23:49:44 -0600

    Day 10

---
+++ Day10.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2015_csharp
+{
+	public static class Day10
+	{
+		public static string LookAndSay(string numbers)
+		{
+			StringBuilder output = new StringBuilder();
+
+			char current = numbers[0];
+			int count = 1;
+
+			for(int i=1; i<numbers.Length; i++)
+			{
+				if(numbers[i] != current)
+				{
+					output.Append(count.ToString());
+					output.Append(current);
+
+					current = numbers[i];
+					count = 1;
+				}
+				else
+				{
+					count++;
+				}
+			}
+
+			output.Append(count);
+			output.Append(numbers[numbers.Length - 1]);
+
+			return output.ToString();
+		}
+
+		public static void Part1()
+		{
+			string numbers = "1113122113";
+
+			for (int i = 0; i < 40; i++)
+			{
+				numbers = LookAndSay(numbers);
+			}
+
+			Console.WriteLine("d10p1: " + numbers.Length);
+		}
+
+		public static void Part2()
+		{
+			string numbers = "1113122113";
+
+			for (int i = 0; i < 50; i++)
+			{
+				numbers = LookAndSay(numbers);
+			}
+
+			Console.WriteLine("d10p2: " + numbers.Length);
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -4,23 +4,26 @@
 	{
 		static void Main(string[] args)
 		{
-			Day4.Part1();
-			Day4.Part2();
+			//Day4.Part1();
+			//Day4.Part2();
 
-			Day5.Part1();
-			Day5.Part2();
+			//Day5.Part1();
+			//Day5.Part2();
 
-			Day6.Part1();
-			Day6.Part2();
+			//Day6.Part1();
+			//Day6.Part2();
 
-			// part 2 calls part 1
-			Day7.Part2();
+			//// part 2 calls part 1
+			//Day7.Part2();
 
-			Day8.Part1();
-			Day8.Part2();
+			//Day8.Part1();
+			//Day8.Part2();
 
-			Day9.Part1();
-			Day9.Part2();
+			//Day9.Part1();
+			//Day9.Part2();
+
+			Day10.Part1();
+			Day10.Part2();
 		}
 	}
 }
